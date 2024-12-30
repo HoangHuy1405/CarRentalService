8cd55bf304340c7e625407922a9f99ffce673f0c
HoangHuy1405 <nguyenmaihoanghuy@gmail.com>
2024-12-30 15:56:10 +0700

    merge

--- CarRental/Data/ApplicationDbContext.cs
+++ CarRental/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace CarRental.Data
 		public DbSet<DriverRide> DriverRides { get; set; }
 		public DbSet<PassengerRide> PassengerRides { get; set; }
 
+        public DbSet<Notification> Notifications { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             base.OnModelCreating(modelBuilder);
             // Configure PassengerRide -> DriverRide relationship
---
+++ CarRental/Models/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRental.Models {
+    public class Notification {
+        public int Id { get; set; }
+        public string UserId { get; set; } // ID of the user
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
